b8e75f67e59cc431f4ce9449b5cd41917b1759e8
Andrew Schwark <schwark.andrew@gmail.com>
2018-05-28 18:36:31 -0500

    github update

--- config/database.js
+++ config/database.js
@@ -1,5 +1,1 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://triplehigh:!Gr34tc0llapse!@ds233300-a0.mlab.com:33300,ds233300-a1.mlab.com:33300/fulcrum-prod?replicaSet=rs-ds233300'}
-} else {
-    module.exports = {mongoURI: 'mongodb://localhost/fulcrum-dev'}
-}
+module.exports = {mongoURI: 'mongodb://localhost/fulcrum-dev'}
